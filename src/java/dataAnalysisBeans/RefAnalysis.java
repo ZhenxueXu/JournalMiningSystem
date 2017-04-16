484e3d065b0324340c2c8141ff0b5fb8c7ad0ac4
Zhenxue Xu <hquxzx@163.com>
2017-04-16 21:02:33 +0800

    第三模块高低引，变化趋势已完成，关键字，基金，作者单位分析后续处理

--- src/java/dataAnalysisBeans/RefAnalysis.java
+++ src/java/dataAnalysisBeans/RefAnalysis.java
@@ -1,20 +1,52 @@
 
 package dataAnalysisBeans;
 
+import JDBCUtils.JDBCUtils;
 import javax.inject.Named;
 import javax.enterprise.context.SessionScoped;
 import java.io.Serializable;
+import java.sql.*;
 
 /**
- *
  * @author Think
- * 被引统计
+ * 第三模块，被引统计
  */
 @Named(value = "refAnalysis")
 @SessionScoped
 public class RefAnalysis implements Serializable {
+    ResultSet year_citation;   //期刊每年总被引次数
+    ResultSet citationSort;    //论文被引次数排序
 
     public RefAnalysis() {
     }
+    
+    public void setData(){
+        Connection conn = null;
+        Statement stat = null;
+        String sql;
+        try{
+            conn = JDBCUtils.getConn();
+            stat = conn.createStatement();
+            sql = "select j_year,sum(j_citation_frequency) from journal_info group by j_year";
+            year_citation = stat.executeQuery(sql);
+            sql = "j_number,j_title,j_citation_frequency from journal_info order by j_citation_frequency DESC";  //用来处理频次分布，高引，低引
+            citationSort = stat.executeQuery(sql);
+            
+            
+        }catch(Exception e){
+            
+        }finally{
+            JDBCUtils.close(null, stat, conn);
+        }       
+    }
+
+    public ResultSet getYear_citation() {
+        return year_citation;
+    }
+
+    public ResultSet getCitationSort() {
+        return citationSort;
+    }
+    
     
 }
