60e15a4bc62983d791032dbd89be98a9d2e78aa2
Zhenxue Xu <hquxzx@163.com>
2017-05-11 09:06:06 +0800

    盒图解决

--- src/java/dataAnalysisBeans/KeywordCoAppearAnalysis.java
+++ src/java/dataAnalysisBeans/KeywordCoAppearAnalysis.java
@@ -2,14 +2,17 @@ package dataAnalysisBeans;
 
 import JDBCUtils.JDBCUtils;
 import com.github.abel533.echarts.series.Graph;
-import com.github.abel533.echarts.series.force.Link;
 import com.github.abel533.echarts.series.force.Node;
 import com.github.abel533.echarts.style.LineStyle;
-import com.github.abel533.echarts.style.itemstyle.Normal;
 import com.google.gson.Gson;
+import info.GraphLink;
 import info.GraphNormal;
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
 import java.util.Set;
 import javax.inject.Named;
 import javax.enterprise.context.Dependent;
@@ -19,6 +22,9 @@ import javax.enterprise.context.Dependent;
 public class KeywordCoAppearAnalysis {
 
     private Graph graph;
+    private List<GraphLink> links;
+    private List<Node> nodess;
+    private Map<String,List> data;
 
     public KeywordCoAppearAnalysis() {
         setAllData();
@@ -30,6 +36,9 @@ public class KeywordCoAppearAnalysis {
         ResultSet rs = null;
         String sql;
         graph = new Graph();
+        links = new ArrayList<>();
+        nodess = new ArrayList<>();
+        data = new HashMap<>();
         try {
             conn = JDBCUtils.getConn();
             stat = conn.createStatement();
@@ -62,15 +71,17 @@ public class KeywordCoAppearAnalysis {
                 }
                 nodes.add(rs.getString(1));
                 nodes.add(rs.getString(2));
-                Link link = new Link();
+                GraphLink link = new GraphLink();
                 link.setSource(rs.getString(1));
                 link.setTarget(rs.getString(2));
-                link.weight(rs.getInt(3));
+                link.setValue(rs.getInt(3));
+                //link.setWeight(rs.getInt(3));
                 LineStyle linestyle  = new LineStyle();
                 GraphNormal normal = new GraphNormal();
                 normal.setWidth((float) (rs.getInt(3)*5.0/max));
                 linestyle.normal(normal);
                 link.lineStyle(linestyle);
+                links.add(link);
                 graph.links(link);
             }
             for (String item : nodes) {
@@ -78,6 +89,7 @@ public class KeywordCoAppearAnalysis {
                 node.setName(item);
                 node.draggable(Boolean.TRUE);
                 node.symbolSize(2);
+                nodess.add(node);
                 graph.data(node);
             }
 
@@ -93,6 +105,12 @@ public class KeywordCoAppearAnalysis {
         Gson gson = new Gson();
         System.out.println(gson.toJson(graph));
         return gson.toJson(graph);
+    }
+    public String getData(){
+        data.put("links", links);
+        data.put("nodes", nodess);
+        Gson gson = new Gson();
+        return gson.toJson(data);
     }
 
 }
